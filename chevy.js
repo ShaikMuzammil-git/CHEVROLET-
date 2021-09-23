9d8b513c0adadc80665a1638d45c281841d07b40
SHAIK MUZAMMIL HASAN <88976680+ShaikMuzammil-git@users.noreply.github.com>
2021-09-23 19:30:47 +0530

    Update chevy.js

--- chevy.js
+++ chevy.js
@@ -1,1 +1,31 @@
 
+function bodyColor(){
+  const carBody = document.getElementById("section8");
+
+const input = document.getElementById("colorPicker"); 
+
+setColor();
+input.addEventListener("input", setColor);
+
+function setColor() {
+	carBody.style.backgroundColor = input.value;
+}
+
+
+}
+
+function topColor(){
+  const carTop1 = document.getElementById("goliathTopColor1");
+  const carTop2 = document.getElementById("goliathTopColor2");
+const input = document.getElementById("colorPicker2"); 
+
+setColor();
+input.addEventListener("input", setColor);
+
+function setColor() {
+	carTop1.style.backgroundColor = input.value;
+  carTop2.style.backgroundColor = input.value;
+
+}
+
+}
